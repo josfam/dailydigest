d44f96b443f2eaa74a5c99b490086d3ed87218a5
josfam <josephamukun@tutanota.com>
2023-07-21 09:05:22 +0300

    Add code for getting a random quote
    A random quote is retrieved from a prepared database
    of quotes and their authors.
    
    Differs from the course in that a csv file was used
    in the course itself.

--- dd_content.py
+++ dd_content.py
@@ -1,5 +1,26 @@
-def get_random_quote():
-    pass
+import sqlite3
+import random
+from pathlib import Path
+
+
+def get_random_quote(quotes_db='quotes.db'):
+    """Gets a random quote from the prepared sqlite database"""
+    default_quote = '"Well begun is half done."\n— Aristotle'
+    if not Path.exists(Path(quotes_db)):
+        return default_quote
+    try:
+        conn = sqlite3.connect('quotes.db')
+        cur = conn.cursor()
+    except (sqlite3.OperationalError, sqlite3.DatabaseError):
+        # return a default quote
+        return default_quote
+    else:
+        get_quotes = """SELECT "quote", "author" FROM "quotes";"""
+        with conn:
+            cur.execute(get_quotes)
+            quotes = cur.fetchall()
+            quote, author = random.choice(quotes)
+        return f'"{quote}"\n— {author}'
 
 
 def get_weather_forecast():
